3705f92bd2acebd0ae97318dffa3b03884ae7d10
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-04-27 15:53:40 +0000

    Deploy runarf/sokoban to github.com/runarf/sokoban.git:gh-pages

---
+++ precache-manifest.61a7eb66b0924f1eef3605fecd171b5a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c80b0e78a2b893d26e47149d4966b60",
+    "url": "/sokoban/index.html"
+  },
+  {
+    "revision": "4887ce7b2a0f5d9237b8",
+    "url": "/sokoban/static/js/2.ef90e60b.chunk.js"
+  },
+  {
+    "revision": "b9f6c1bc9e30b7a9876c7c17a7a9b5a5",
+    "url": "/sokoban/static/js/2.ef90e60b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4bf6ee41af92609821db",
+    "url": "/sokoban/static/js/main.cb16bfa9.chunk.js"
+  },
+  {
+    "revision": "6086fb2970deddd63997",
+    "url": "/sokoban/static/js/runtime-main.71974546.js"
+  }
+]);
